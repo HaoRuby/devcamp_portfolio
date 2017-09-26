2e982a1eaf8a7437f77892c645b94d581d76eb47
HaoNguyen <haont@hoatamus.com>
2017-09-26 13:58:58 +0700

    add enum status to Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ApplicationRecord
+  # define enum status for blog: draft or published
+  enum status: { draft: 0, published: 1 }
+  # use friendly_id: show title in url instead of id
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
