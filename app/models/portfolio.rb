c882448dbedb75e3f481432113a3762ed3ebe637
HaoNguyen <haont@hoatamus.com>
2017-09-27 09:47:38 +0700

    set default data for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,12 @@ class Portfolio < ApplicationRecord
   end
   # or can use this way
   # scope :angular, -> { where(subtitle: 'angular') }
+
+  # Setting default data
+  after_initialize :set_default
+
+  def set_default
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
