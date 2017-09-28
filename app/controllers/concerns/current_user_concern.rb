d7cd042e3f09bba4814730e04abf3d5563f3144c
HaoNguyen <haont@hoatamus.com>
2017-09-28 15:57:12 +0700

    add permission to Blog

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,12 +2,15 @@ module CurrentUserConcern
   extend ActiveSupport::Concern
 
   # Overide devise current_user
-  # Return: User if login else return an OpenStruct object if not login
+  # Return: User if login else return an guest user object if not login
   def current_user
     super || guest_user
   end
 
   def guest_user
-    OpenStruct.new(name: 'Guest User')
+    guest = GuestUser.new
+    guest.name = 'Guest User'
+    guest.email = 'guestuser@example.com'
+    guest
   end
 end
